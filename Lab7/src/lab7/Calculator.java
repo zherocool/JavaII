46636891030a7e03209552128f12b889935d3737
John Thomas <chainsawgoblin@yahoo.com>
2013-04-01 11:21:07 -0500

    Saving to continue later

--- Lab7/src/lab7/Calculator.java
+++ Lab7/src/lab7/Calculator.java
@@ -1,5 +1,25 @@
 package lab7;
 
-public class Calculator {
+import java.util.List;
+import java.util.Scanner;
 
+public class Calculator {
+	
+	public static void main(String[] args) {
+		System.out.println(Constants.WELCOME_MESSAGE);
+		
+		startInputLoop();
+	}
+	
+	public static void startInputLoop() {
+		Scanner scanner = new Scanner(System.in);
+		
+		String[] input = scanner.next().split(Constants.DELIMETER);
+		boolean stop = true; //input. .contains(Constants.QUIT_SEQUENCE);
+		
+		
+		while (!stop) {
+			
+		}
+	}
 }
---
+++ Lab7/src/lab7/Constants.java
@@ -0,0 +1,7 @@
+package lab7;
+
+public final class Constants {
+	public static final String WELCOME_MESSAGE = "Starting Calculator; Enter an expression or Q to quit";
+	public static final CharSequence QUIT_SEQUENCE = "q";
+	public static final String DELIMETER = " ";
+}
